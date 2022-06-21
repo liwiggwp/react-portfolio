cd37525df53a71e54f08940c787fc0be18255bbb
liwi <liwiggwp@gmail.com>
2022-06-22 01:22:52 +0500

    add component and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './Components/header/Header'
+import Nav from './Components/nav/Nav'
+import About from './Components/about/About'
+import Experience from './Components/experience/Experience'
+import Portfolio from './Components/portfolio/Portfolio'
+import Testimonials from './Components/testimonials/Testimonials'
+import Contact from './Components/contact/Contact'
+import Footer from './Components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+        <Header/>
+        <Nav/>
+        <About/>
+        <Experience/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+    </>
   )
 }
 
---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+          <h5>Hello</h5>
+          <h1>Venera Kodo</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+      </div>
+    </header>
+  )
+}
+
+export default Header
